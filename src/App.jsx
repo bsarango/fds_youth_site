1d280ce8245dbae755177c64c33137ef8f826de7
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-11-25 15:36:26 -0500

    Home component updated - div and p tag created; Home imported to App.jsx  - <> tag replaced for div tag

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <p>FDS Youth Website!</p>
-      </div>
-    </>
+    <div>
+      <Home/>
+    </div>
   )
 }
 
