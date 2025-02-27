1a26270d9cc0e43cf1323a2cbfb3613120ef8fa7
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2025-02-27 14:48:14 -0500

    div tag with inner a tags added - links for social media links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,18 @@ function Home(){
             <h3 className="bio_title">Who we are</h3>
             <p>
                 We're a group of young people who are passionate about seeking God <br></br>
-                and helping other youth know, grow, and learn in the Lord. We would love for 
+                and helping other youth know, grow, and learn in the Lord. <br></br>
+                 Our mission is to help Youth reach heaven. We would love for 
                 you join us in growing in God!
             </p>
             <h3 className= "announcements_heading">Announcements</h3>
             <h4 className='socials_heading'>Follow Us</h4>
             <h4 className='socials_heading'>Socials</h4>
+            <div>
+                {/* Make these all icons */}
+                <a href="">Instagram</a>
+                <a href="https://www.facebook.com/FDSyouthministry/" alt="Facebook">Facebook</a> 
+            </div>
         </div>
     )
 }
