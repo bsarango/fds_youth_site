0c11323e31d86741404f2e323c43f98fb9e43516
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2025-02-27 14:41:29 -0500

    classNames assigned to h2 and h3 tags in div in Home; h3 and h4 tags created as headings in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,16 @@ function Home(){
     return(
         <div>
             <Navbar/>
-            <h2>Welcome to FDS Youth</h2>
-            <h3 className='subtitle'>Who we are</h3>
+            <h2 className="title">Welcome to FDS Youth</h2>
+            <h3 className="bio_title">Who we are</h3>
             <p>
                 We're a group of young people who are passionate about seeking God <br></br>
                 and helping other youth know, grow, and learn in the Lord. We would love for 
                 you join us in growing in God!
             </p>
+            <h3 className= "announcements_heading">Announcements</h3>
+            <h4 className='socials_heading'>Follow Us</h4>
+            <h4 className='socials_heading'>Socials</h4>
         </div>
     )
 }
