c0cfe8358cdbfe39acecb31f7090c6d26a2b9570
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2025-03-06 16:06:09 -0500

    state hook imported to Annoucnements component; displayAnnoucements function created to display Announcment components made from data; state variables created for announcements

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{state} from 'react'
 import Announcement from 'Announcement'
 
 function Announcement(){
+
+    const [announcements, setAnnouncements] = useState([])
+
+    function displayAnnouncements(){
+        //Use a loop/method to convert data into an announcement component and add to an annoucement state variable
+    }
+
     return(
-        <div>
-            
+        <div className='announcements_container'>
+            {/* Get a list of all announcements */}
         </div>
     )
 }
